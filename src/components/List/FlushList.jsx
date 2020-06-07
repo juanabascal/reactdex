95237f17890cb9a9e3089d640155a76b4340d075
Juan <juanabascalsanchez@gmail.com>
2020-06-07 23:17:22 +0200

    Add modal

--- src/components/List/FlushList.jsx
+++ src/components/List/FlushList.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useModal from "../../hooks/useModal";
+import Modal from "../modal/Modal";
 
 const FlushList = ({ elements, icon }) => {
+  const { isShowing, toggle } = useModal();
+
   return (
     <>
       <ul className="list-group list-group-flush">
@@ -9,13 +13,23 @@ const FlushList = ({ elements, icon }) => {
           <li className="list-group-item" key={element.id}>
             {element.name}
             {icon && (
-              <span className="float-right">
+              <span
+                className="float-right"
+                style={{ cursor: "pointer" }}
+                onClick={toggle}
+              >
                 <FontAwesomeIcon icon={icon} />
               </span>
             )}
           </li>
         ))}
       </ul>
+      <Modal
+        isShowing={isShowing}
+        hide={toggle}
+        header="Blaze"
+        text="This should be set dynamically."
+      />
     </>
   );
 };
---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./Modal.module.css";
+
+const Modal = ({ isShowing, hide, header, text }) =>
+  isShowing
+    ? ReactDOM.createPortal(
+        <React.Fragment>
+          <div className={styles.overlay} />
+          <div
+            className={styles.wrapper}
+            aria-customModal
+            aria-hidden
+            tabIndex={-1}
+            role="dialog"
+          >
+            <div className={styles.modal}>
+              <div className={styles.modalHeader}>
+                <button
+                  type="button"
+                  className={styles.closeButton}
+                  data-dismiss="modal"
+                  aria-label="Close"
+                  onClick={hide}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <h3>{header}</h3>
+              <p>{text}</p>
+            </div>
+          </div>
+        </React.Fragment>,
+        document.body
+      )
+    : null;
+
+export default Modal;
